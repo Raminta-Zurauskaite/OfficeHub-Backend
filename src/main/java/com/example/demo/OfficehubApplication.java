e602801dd311237d82d6014160bee09e34d47be1
Raminta <ramintules@gmail.com>
2022-04-05 19:11:40 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/OfficehubApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficehubApplication.class, args);
+	}
+
+}
