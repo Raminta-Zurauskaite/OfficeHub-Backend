f13da7a2c5566c0cbe0468d8b146bc39ac7e5b9e
Laurynas Plokštys <laurynas.plok5@gmail.com>
2022-04-07 15:50:54 +0300

    third try for postgresql

---
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.UserEntity;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserEntity saveUserEntity(UserEntity userEntity){
+        return userRepository.save(userEntity);
+    }
+}
